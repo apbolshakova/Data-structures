56dcba5a46ba821e34110a83ecfd36f2d6820c27
Анжела Большакова <bolshakova2.ap@edu.spbstu.ru>
2019-03-09 23:04:23 +0300

    Добавлен тип, отвечающий за успешность операций, интегрирован в имеющийся код (добавлены обработки ошибок)

--- expressionWithBrackets/expressionWithBrackets/expressionWithBrackets.cpp
+++ expressionWithBrackets/expressionWithBrackets/expressionWithBrackets.cpp
@@ -3,6 +3,8 @@
 
 typedef enum Bool_ { FALSE, TRUE } bool_t;
 
+typedef enum FuncResult_ { FAIL, SUCCESS } func_result_t;
+
 typedef struct Number_
 {
 	char* asString;
@@ -16,12 +18,12 @@ typedef union OpzElValue_
 	char sign;
 	number_t* number;
 } opz_list_el_value;
+
 typedef struct OpzEl_
 {
 	opz_list_el_value* value;
 	struct OpzEl_* next;
 } opz_list_el;
-/*Линейный список ОПЗ*/
 
 /*структура стека символов, используемая при получении ОПЗ*/
 typedef struct OperatorStackEl_
@@ -32,30 +34,80 @@ typedef struct OperatorStackEl_
 
 /*TODO: структура стека для подсчёта опз*/
 
-bool_t isDigit(char symbol)
+bool_t isDigit (char symbol)
 {
 	if ('0' <= symbol && symbol <= '9') return TRUE;
 	else return FALSE;
 }
 
-bool_t ifOperator(char symbol)
+bool_t ifOperator (char symbol)
 {
 	if (symbol == '+' || symbol == '-' || symbol == '*' || symbol == '/') return TRUE;
 	else return FALSE;
 }
 
-void handleOpzListValue(opz_list_el** opzList_headPtr, operator_stack_el** operatorStack_headPtr, char** curChar)
+func_result_t handleNumber(opz_list_el** opzList_headPtr, char** curChar)
+{
+	//распарсить число в структуру и добавить в список ОПЗ
+}
+
+func_result_t handleOparator(opz_list_el** opzList_headPtr, operator_stack_el** operatorStack_headPtr, char** curChar)
+{
+	//вытащить из стека все операции с большим приоритетом до первой открывающей скобки не включительно
+	//пишем полученный оператор в стек
+}
+
+func_result_t handleOpeningBracket(operator_stack_el** operatorStack_headPtr)
+{
+	//положить скобку в стек
+}
+
+func_result_t handleClosingBracket(opz_list_el** opzList_headPtr, operator_stack_el** operatorStack_headPtr)
+{
+	//вытащить из стека в ОПЗ все операции до открывающей скобки включительно
+}
+
+func_result_t handleOpzListValue(opz_list_el** opzList_headPtr, operator_stack_el** operatorStack_headPtr, char** curChar)
 {
 	if (curChar == NULL) return NULL;
 	opz_list_el_value* elValue = (opz_list_el_value*)malloc(sizeof(opz_list_el_value));
 
-	if      (isDigit(**curChar))    handleNumber(opzList_headPtr, operatorStack_headPtr, curChar);
-	else if (isOperator(**curChar)) handleOperator(opzList_headPtr, operatorStack_headPtr);
-	else if (**curChar == '(') handleOpeningBracket(opzList_headPtr, operatorStack_headPtr);
-	else if (**curChar == ')') handleClosingBracket(opzList_headPtr, operatorStack_headPtr);
+	if (isDigit(**curChar))
+	{
+		if (handleNumber(opzList_headPtr, curChar) != SUCCESS)
+		{
+			printf("ERROR: found incorrect number.\n");
+			return FAIL;
+		};
+	}
+	else if (isOperator(**curChar))
+	{
+		if (handleOperator(opzList_headPtr, operatorStack_headPtr, curChar) != SUCCESS) //TODO: узнать, почему может быть ошибка
+		{
+			printf("ERROR: problem with operators stack.\n");
+			return FAIL;
+		}
+	}
+	else if (**curChar == '(')
+	{
+		if (handleOpeningBracket(operatorStack_headPtr) != SUCCESS)
+		{
+			printf("ERROR: problem with operators stack.\n");
+			return FAIL;
+		}
+	}
+	else if (**curChar == ')')
+	{
+		if (handleClosingBracket(opzList_headPtr, operatorStack_headPtr) != SUCCESS)
+		{
+			printf("ERROR: number of opening and closing brackets is unbalanced.\n");
+			return FAIL;
+		}
+	}
+	return SUCCESS;
 }
 
-opz_list_el* getOpz() //читает входные данные, парсит их в ОПЗ и возвращает указатель на голову полученного списка
+opz_list_el* getOpz () //читает входные данные, парсит их в ОПЗ и возвращает указатель на голову полученного списка (вернёт NULL в случае ошибки)
 {
 	opz_list_el* opzList_head = NULL;
 	operator_stack_el* operartorStack_head = NULL;
@@ -64,23 +116,26 @@ opz_list_el* getOpz() //читает входные данные, парсит их в ОПЗ и возвращает указ
 	if (!scanf("%c", &curChar)) return opzList_head;
 	while (curChar != '=')
 	{
-		handleOpzListValue(&opzList_head, &operartorStack_head, &curChar); //добавляет новые элементы в список ОПЗ в соот-вии с семантикой
-		if (!scanf("%c", &curChar)) return NULL; //выражение без знака равно => ошибка. TODO: возвращать код ошибки??
+		if (handleOpzListValue(&opzList_head, &operartorStack_head, &curChar) != SUCCESS) //добавляет новые элементы в список ОПЗ в соот-вии с семантикой
+		{
+			printf("ERROR: impossible to build correct RPN.\n");
+			return NULL;
+		}
+		if (!scanf("%c", &curChar))
+		{
+			printf("ERROR: '=' sign not found.\n");
+			return NULL;
+		}
 		
 	}
-	//динамически читать строку, пока есть, при первом проходе формируем opz_list:
-	/*
-	открывающая скобка - скидываем в стек
-	закрывающая скобка - вытаскиваем из стека всё до первой открывающей скобки включительно
-	число обрабатываем и скидываем в экземпляр number_t
-	символ - вытаскиваем все операции с большим приоритетом до первой открывающей скобки
-	конец строки - вытаскиваем всё из стека символов
-	*/
+	if (popRestOfOperatorStack(&opzList_head, &operartorStack_head) != SUCCESS) return NULL; //вытащить всё, что осталось в стеке операторов
+
+	return opzList_head;
 }
 
-int main(void)
+int main (void)
 {
-	printf("Введите выражение для подсчёта (должно заканчиваться знаком равенства): \n");
+	printf("Enter expression to calculate (it must end with '='):\n");
 	opz_list_el* opzListHead = getOpz(); //получить опз
 	calculateOpz(opzListHead); //поссчитать выражение по опз
 	return 0;
