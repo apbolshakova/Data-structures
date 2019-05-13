685e3c2adbe71fa3e822534d2e9d4dc59c3f778d
julody <julody@mail.ru>
2019-05-13 11:27:21 +0300

    Реализована applyVerChanges и printText

--- Git/Git/Text.c
+++ Git/Git/Text.c
@@ -41,7 +41,7 @@ func_res_t print()
 		printf("ERROR: unable to print text.\n");
 		return FAIL;
 	}
-	//printText(text); TODO
+	printText(text);
 	free(text);
 	return SUCCESS;
 }
@@ -54,11 +54,11 @@ func_res_t getCurText(char* text)
 		printf("ERROR: unable to get path to buffer.\n");
 		return FAIL;
 	}
-	/*if (applyChanges(text, pathToBuf) == FAIL)
+	if (applyChanges(text, pathToBuf) == FAIL)
 	{
 		printf("ERROR: unable to apply all operations that were made.\n");
 		return FAIL;
-	}*/
+	}
 	//TODO: not forget to clean pathToBuf
 	return SUCCESS;
 }
@@ -73,7 +73,27 @@ func_res_t applyChanges(char* text, path_t* el)
 	return SUCCESS;
 }
 
-func_res_t applyVerChanges(char* text, operation_t* opList)
+func_res_t applyVerChanges(char* text, operation_t* opEl)
 {
-	//TODO
+	while (opEl)
+	{
+		     if (opEl->type == '+') addToText(text, opEl);
+		else if (opEl->type == '-') removeFromText(text, opEl);
+		else
+		{
+			printf("ERROR: invalid operation found.\n");
+			return FALSE;
+		}
+		opEl = opEl->next;
+	}
+	return SUCCESS;
+}
+
+void printText(char* text)
+{
+    while (*text)
+	{
+		printf("%c", *text);
+		text++;
+	}
 }
